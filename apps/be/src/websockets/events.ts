7613461da1eb2f52c58d31fde1735370466f2636
Diogo <diogocorreia1710@gmail.com>
2024-12-08 15:40:33 +0000

    be: lint and deploy workflow

--- apps/be/src/websockets/events.ts
+++ apps/be/src/websockets/events.ts
@@ -1,5 +1,6 @@
+import { PaymentType } from "../payments/model/payment.model";
 import { broadcast } from "./websocketServer";
 
-export const broadcastNewPayment = (payment: any) => {
+export const broadcastNewPayment = (payment: PaymentType) => {
     broadcast({ type: "NEW_PAYMENT", data: payment });
 };
